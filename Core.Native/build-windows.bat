e57d8ff9274350f989deeb29bbcf2adecc471c37
Virgile Bello <virgile@siliconstudio.co.jp>
2015-07-21 13:29:15 +0900

    Release 1.2.0-beta

--- Core.Native/build-windows.bat
+++ Core.Native/build-windows.bat
@@ -6,4 +6,12 @@ msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Confi
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsStore;Platform=ARM
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsPhone;Platform=Win32
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsPhone;Platform=ARM
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\vc\vcvarsall.bat" x86
+@REM These variables are set by VCVarsQueryRegistry.bat and need to be cleared (as of VS2015 RC)
+@set WindowsSdkDir=
+msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows10;Platform=x64
+msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows10;Platform=Win32
+msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows10;Platform=ARM
+
 xcopy /Y /S ..\..\core\SiliconStudio.Core.Native\bin\*.dll .
