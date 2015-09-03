519cc855bec245466bab95d137b8bc00cd645941
Virgile Bello <virgile@siliconstudio.co.jp>
2015-09-03 16:17:18 +0900

    [Core] Updated to VS2015

--- Core.Native/build-windows.bat
+++ Core.Native/build-windows.bat
@@ -1,4 +1,5 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\vc\vcvarsall.bat" x86
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\vc\vcvarsall.bat" x86
+@REM These variables are set by VCVarsQueryRegistry.bat and need to be cleared (as of VS2015 RC)
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows;Platform=Win32
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows;Platform=x64
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsStore;Platform=x64
@@ -6,10 +7,6 @@ msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Confi
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsStore;Platform=ARM
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsPhone;Platform=Win32
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=WindowsPhone;Platform=ARM
-
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\vc\vcvarsall.bat" x86
-@REM These variables are set by VCVarsQueryRegistry.bat and need to be cleared (as of VS2015 RC)
-@set WindowsSdkDir=
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows10;Platform=x64
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows10;Platform=Win32
 msbuild ..\..\core\SiliconStudio.Core.Native\Core.Native.vcxproj /Property:Configuration=Release;SiliconStudioPlatform=Windows10;Platform=ARM
